1de785e2ac80c99a00af01016cd5ed7b37a46aea
lyonsm <michael.lyons@colostate.edu>
2020-08-03 15:17:26 -0600

    updated project_mkdir to current version

--- misc_notes/project_mkdir.sh
+++ misc_notes/project_mkdir.sh
@@ -20,20 +20,23 @@ proj_gitignore=$2
 # Project directory 
 proj_dir_name=$PROJECT_DIR$proj_name  
 
+  mkdir -p  $proj_dir_name/bin
   mkdir -p  $proj_dir_name/data
   mkdir -p  $proj_dir_name/doc/manuscript
   mkdir -p  $proj_dir_name/doc/notes
   mkdir -p  $proj_dir_name/results
-  mkdir -p  $proj_dir_name/src/mcsim
-  mkdir -p  $proj_dir_name/src/nsga
+  mkdir -p  $proj_dir_name/src/C/mcsim/sim/
+  mkdir -p  $proj_dir_name/src/C/nsga
   mkdir -p  $proj_dir_name/src/R
   mkdir -p  $proj_dir_name/src/sql
   mkdir -p  $proj_dir_name/src/gnuplot
+  mkdir -p  $proj_dir_name/src/shell
 
 # Project description text file
   touch $proj_dir_name/"$proj_name"_"$timestamp.txt"
 
 # Initialize a git repository for version control
   git init $proj_dir_name
+
 # Copy an initial gitignore file to project directory
   cp $proj_gitignore $proj_dir_name/.gitignore
