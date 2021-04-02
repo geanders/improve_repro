e9f7609154bfd29a4c473df53dc42579837ac925
Brooke Anderson <brooke.anderson@colostate.edu>
2021-04-02 10:28:57 -0600

    Make some edits to protocol chapter

--- data/bactcountr_example_data/data_to_csv.R
+++ data/bactcountr_example_data/data_to_csv.R
@@ -1,18 +1,5 @@
 library(tidyverse)
 
-# First version---scenario of recording data in tidy format
-
-load("data/bactcountr_example_data/CFU_raw_formatted.rda")
-
-cfu_data_tidy <- CFU_raw_formatted %>% 
-  filter(organ == "Spleen") %>% 
-  select(-organ) %>% 
-  mutate(replicate = paste(group, replicate, sep = "-"))
-
-write_csv(cfu_data, file = "data/bactcountr_example_data/cfu_data_tidy.csv")
-
-# Second version---tidy data after reading it in
-
 load("data/bactcountr_example_data/CFU_data.rda")
 
 cfu_data <- CFU_data %>% 
